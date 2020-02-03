920399529d455b6ac8a33e68374adc9e8bc51c90
Ewan Dawson <ewan.dawson@symphonicsoft.com>
2020-02-03 13:31:44 +0000

    Updated evernote-api-dependency to latest version

--- clojurenote/project.clj
+++ clojurenote/project.clj
@@ -1,4 +1,4 @@
-(defproject clojurenote "0.4.0"
+(defproject clojurenote "0.4.1"
   :description "Clojure library to access Evernote API"
   :url "https://github.com/mikebroberts/clojurenote"
   :license {:name "The MIT License"
@@ -6,7 +6,7 @@
   :dependencies [
     [org.clojure/clojure "1.4.0"]
     [org.scribe/scribe "1.3.5"]
-    [com.evernote/evernote-api "1.25"]
+    [com.evernote/evernote-api "1.25.1"]
   ]
   :profiles {
     :dev {
